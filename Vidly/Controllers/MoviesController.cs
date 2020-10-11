710b763836d16af71026e9070e8809274268d902
shariful-islam-sharif <imdshariful171@gmail.com>
2020-10-11 17:40:42 +0600

    initial commit

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { name = "Avengers" };
+            return View(movie);
+        }
+        // GET: Movies/Edit
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }// GET: Movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if(!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (string.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "name";
+            }
+            return Content(string.Format("pageIndex={0}&sortBy={1}",pageIndex,sortBy));
+        }
+    }
+}
